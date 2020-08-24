66557cf0691a1a8eb8a1d390b4abbca838d48c63
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-08-24 10:44:02 +0800

    update signin page

--- src/layoutComponents/Navbar.js
+++ src/layoutComponents/Navbar.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { imageStyle, mainStyle } from '../style/style.js'
 import { signInWithGoogle, signInWithEmail, signOut } from '../firebase/Config'
 import { Link } from "react-router-dom"
-import { Navbar, Nav, Button, Image, Dropdown, DropdownButton } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Button, Image } from 'react-bootstrap'
 
 const NavbarConent = ({ currentUser, setPage }) => {
 
   return (
     <Navbar bg='light' style={mainStyle} collapseOnSelect expand="lg">
-      <Navbar.Brand className='navColor' href="/">Navbar</Navbar.Brand>
+      <Navbar.Brand className='navColor' href="/">React CRUD</Navbar.Brand>
       {
         currentUser &&
           <Image
+            href="/"
             className='m-2'
             style={imageStyle}
             rounded
@@ -21,27 +22,21 @@ const NavbarConent = ({ currentUser, setPage }) => {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto navColor">
-          <Nav.Link href="/">Profile</Nav.Link>
-          <Nav.Link href="/contacts">Contacts</Nav.Link>
           {
             currentUser ?
-            <Button size='sm' className='mt-1' variant="outline-primary" onClick={signOut}>
-              LogOut
-            </Button> :
-            <DropdownButton size='sm' className='mt-1' variant="outline-primary" title="LogIn">
-              <Dropdown.Item
-                as="button"
-                variant="outline-primary"
-                onClick={signInWithGoogle}>
-                  Google Login
-              </Dropdown.Item>
-              <Dropdown.Item
-                as="button"
-                variant="outline-primary"
-                onClick={() => setPage('SignIn')}>
-                  SignIn
-              </Dropdown.Item>
-            </DropdownButton>
+            <>
+              <Nav.Link href="/">Profile</Nav.Link>
+              <Nav.Link href="/contacts">Contacts</Nav.Link>
+              <Nav.Link onClick={signOut}>Log Out</Nav.Link>
+            </> :
+            <NavDropdown title="Log In" >
+              <NavDropdown.Item onClick={signInWithGoogle}>
+                Google Login
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={() => setPage('SignIn')}>
+                SignIn
+              </NavDropdown.Item>
+            </NavDropdown>
           }
         </Nav>
       </Navbar.Collapse>
@@ -50,13 +45,3 @@ const NavbarConent = ({ currentUser, setPage }) => {
 }
 
 export default NavbarConent
-
-
-// <nav style={navStyle} className='py-3 mb-3'>
-//   <Button className='m-2' variant="outline-primary" onClick={() => setPage('Profile')}>Profile</Button>
-//   <Button className='m-2' variant="outline-primary" onClick={() => setPage('Contacts')}>Contacts</Button>
-// </nav>
-
-// <Button className='m-2' variant="outline-primary" onClick={signInWithGoogle}>
-//   LogIn
-// </Button>
--- src/pageComponents/SignIn/SignIn.js
+++ src/pageComponents/SignIn/SignIn.js
@@ -1,15 +1,63 @@
-import React from 'react'
-import { Jumbotron } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { jumbotronStyle } from '../../style/style'
+import { Container, Row, Col, Jumbotron, Form, Button } from 'react-bootstrap'
 
-const SignIn = () => {
+const SignIn = ({ addOrEdit }) => {
+
+  const initialFieldValues = {
+    email: '',
+    password: '',
+  }
+
+  const [values, setValues] = useState(initialFieldValues)
+
+  const handlerInputChange = (e) => {
+    const { name, value } = e.target
+    setValues({
+      // 這邊先串接useState中的valuses，然後使用陣列去寫入每個input裡面的name的值
+      ...values,
+      [name]: value
+    })
+  }
+
+  const handlerFormSubmit = (e) => {
+    e.preventDefault()
+    addOrEdit(values)
+  }
 
   return (
-    <Jumbotron>
-      <h1>Sign in with your eamil</h1>
-      <p>
-        You have to log in to unlock more information.
-      </p>
-    </Jumbotron>
+    <Container>
+      <Row>
+        <Col xs={12} sm={12} md={10} lg={8} xl={6}>
+          <Jumbotron style={jumbotronStyle}>
+            <h1>Sign in with your eamil</h1>
+            <Form.Group controlId='formEmail'>
+              <Form.Label>Email</Form.Label>
+              <input
+                className='form-control'
+                placeholder='Input E-mail'
+                name='email'
+                value={values.email}
+                onChange={handlerInputChange} />
+            </Form.Group>
+
+            <Form.Group controlId='formPassword'>
+              <Form.Label>Password</Form.Label>
+              <input
+                type='password'
+                className='form-control'
+                placeholder='Input password'
+                name='password'
+                value={values.password}
+                onChange={handlerInputChange} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
