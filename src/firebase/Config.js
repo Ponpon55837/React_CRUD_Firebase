c15fc7c10f367ce1f17aa537ba00d66a15a5bf46
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-08-30 13:41:12 +0800

    add env

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -5,17 +5,19 @@ import 'firebase/storage'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAYxBKTuy9fUOz3lGMExaUfVYEoPREDANs",
-    authDomain: "react-crud-fcfb1.firebaseapp.com",
-    databaseURL: "https://react-crud-fcfb1.firebaseio.com",
-    projectId: "react-crud-fcfb1",
-    storageBucket: "react-crud-fcfb1.appspot.com",
-    messagingSenderId: "206599953779",
-    appId: "1:206599953779:web:8eeba1c73e061611bb7906",
-    measurementId: "G-XJKY6R01YT"
-  };
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  }
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
+
+  console.log(firebaseConfig)
 
   export const projectDB = firebase.database().ref()
   export const porjectAuth = firebase.auth()
