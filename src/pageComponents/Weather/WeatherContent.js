e1343e90cfba68d9c31a1364ffb4e59c6583f129
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-10-01 10:53:43 +0800

    add new content

--- src/pageComponents/Weather/WeatherContent.js
+++ src/pageComponents/Weather/WeatherContent.js
@@ -6,28 +6,31 @@ import { ReactComponent as AirFlowIcon } from './images/airFlow.svg'
 import { ReactComponent as RainIcon } from './images/rain.svg'
 import { ReactComponent as RefreshIcon } from './images/refresh.svg'
 
-const WeatherContent = () => {
+const WeatherContent = ({ currentWeather }) => {
 
   return (
     <Container>
       <Row>
-        <Col xs={12} md={6} style={location}>Taipei</Col>
-        <Col xs={11} sm={11} md={5} style={description}>多雲時晴</Col>
-        <Col xs={1} sm={1}vmd={1}>
+        <Col xs={12} md={12} style={location}>{currentWeather.locationName}</Col>
+        <Col xs={5} sm={5} md={5}>{currentWeather.observationTime}</Col>
+        <Col xs={4} sm={4} md={4} style={description}>
+          {currentWeather.description}
+        </Col>
+        <Col xs={1} sm={1} md={1}>
           <RefreshIcon style={refreshSvg} />
         </Col>
       </Row>
       <Row>
         <Col xs={12} md={6} lg={4} xl={4} style={temperature}>
-          <div className='mr-2'>23°C</div>
+          <div className='mr-2'>{Math.round(currentWeather.temperature)}°C</div>
           <CloudyIcon style={styleSvg} />
         </Col>
         <Col xs={12} md={6} lg={4} xl={4} style={airFlow}>
-          <div className='mr-2'>23 m/h</div>
+          <div className='mr-2'>{currentWeather.windSpeed} m/h</div>
           <AirFlowIcon style={styleSvg} />
         </Col>
         <Col xs={12} md={6} lg={4} xl={4} style={rain}>
-          <div className='mr-2'>48%</div>
+          <div className='mr-2'>{currentWeather.humid * 100}%</div>
           <RainIcon style={styleSvg} />
         </Col>
       </Row>
--- src/pageComponents/Weather/WeatherProfile.js
+++ src/pageComponents/Weather/WeatherProfile.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WeatherContent from './WeatherContent'
 import { jumbotronStyle } from '../../style/style'
 import { Jumbotron, Card } from 'react-bootstrap'
 
 const WeatherProfile = () => {
+  const initialValues = {
+    observationTime: '2019-10-02 22:10:00',
+    locationName: '臺北市',
+    description: '多雲時晴',
+    temperature: 27.5,
+    windSpeed: 0.3,
+    humid: 0.88
+  }
+
+  const [currentWeather, setCurrentWeather] = useState(initialValues)
 
   return (
     <Jumbotron style={jumbotronStyle} className='m-5'>
       <h1 className='mb-5'>This is Weather Part</h1>
         <Card>
           <Card.Body>
-            <WeatherContent />
+            <WeatherContent currentWeather={currentWeather} />
           </Card.Body>
         </Card>
     </Jumbotron>
