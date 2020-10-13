bfa543df9fe58951fd6c7292ef0532d4827a7cdf
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-10-13 11:20:55 +0800

    fix weather cannot fit with all weather state problem

---
+++ src/pageComponents/Weather/WeatherDescription.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ReactComponent as ClearIcon } from './images/day-clear.svg'
+import { ReactComponent as CloudyIcon } from './images/day-cloudy.svg'
+import { ReactComponent as CloudyFOGIcon } from './images/day-cloudy-fog.svg'
+import { ReactComponent as DayFOGIcon } from './images/day-fog.svg'
+
+const WeatherDescription = ({ currentWeather, styleSvg }) => {
+
+  const descriSwitch = () => {
+    switch(currentWeather.description) {
+      case '天氣：晴':
+        return (<ClearIcon style={styleSvg} />)
+      case '天氣：多雲':
+        return (<CloudyIcon style={styleSvg} />)
+      case '天氣：多雲時晴':
+        return (<CloudyIcon style={styleSvg} />)
+      case '天氣：晴有霾':
+        return (<DayFOGIcon style={styleSvg} />)
+      case '天氣：陰':
+        return (<CloudyFOGIcon style={styleSvg} />)
+    }
+  }
+
+  return (
+    <>
+      {descriSwitch()}
+    </>
+  )
+}
+
+export default WeatherDescription
